0176668f2986a250e3a2fc7a802bca606d7aacf4
ek93922 <108656846+ek93922@users.noreply.github.com>
2023-03-07 12:41:46 -0800

    Add files via upload

--- xlsxToxls.py
+++ xlsxToxls.py
@@ -8,13 +8,19 @@ def main():
 
     os.chdir(os.path.abspath(root_dir))
     pdir = Path(root_dir)
-    filelist = [filename for filename in pdir.iterdir() if filename.suffix == ".xlsx"]
+    # Creates a list of files that ends in '.xlsx'
+    xlsx_filelist = [filename for filename in pdir.iterdir() if filename.suffix == ".xlsx"]
+    # Creates a list of files that ends in '.xls'
+    xls_filelist = [filename for filename in pdir.iterdir() if filename.suffix == ".xls"]
 
-
-    for xlsxfile in filelist:
-        workbook = openpyxl.load_workbook(xlsxfile)
-        outfile = f"{xlsxfile.name.split('.')[0]}.xls"
-        workbook.save(outfile)
+    for xlsxfile in xlsx_filelist:
+        # Pre-determines the new file name
+        output = f"{xlsxfile.name.split('.')[0]}.xls"
+        # Prevents duplicate files from being created
+        # after first successful conversion
+        if output not in xls_filelist:
+            workbook = openpyxl.load_workbook(xlsxfile)
+            workbook.save(output)
 
 if __name__ == '__main__':
     main()
